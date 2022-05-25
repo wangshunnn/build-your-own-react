02c54d169d2e07b6ae556be822f607d27d88bce4
Wang Shun <103867940+wangshunnn@users.noreply.github.com>
2022-05-25 10:36:10 +0800

    Create step-II-the-render-function.js

---
+++ steps/step-II-the-render-function.js
@@ -0,0 +1,19 @@
+function render(element, container) {
+    const dom =
+        element.type == "TEXT_ELEMENT"
+            ? document.createTextNode("")
+            : document.createElement(element.type);
+    const isProperty = (key) => key !== "children";
+    Object.keys(element.props)
+        .filter(isProperty)
+        .forEach((name) => {
+            dom[name] = element.props[name];
+        });
+    element.props.children.forEach((child) => render(child, dom));
+    container.appendChild(dom);
+}
+
+const Didact = {
+    createElement,
+    render
+};

54354734e0a50cea6002c43619eb0596ad8ecaad
Wang Shun <103867940+wangshunnn@users.noreply.github.com>
2022-05-26 22:44:49 +0800

    Create step-VIII-hooks.js

---
+++ steps/step-VIII-hooks.js
@@ -0,0 +1,59 @@
+let wipFiber = null
+let hookIndex = null
+
+function updateFunctionComponent(fiber) {
+    wipFiber = fiber
+    hookIndex = 0
+    wipFiber.hooks = []
+    const children = [fiber.type(fiber.props)]
+    reconcileChildren(fiber, children)
+}
+
+function useState(initial) {
+    const oldHook =
+        wipFiber.alternate &&
+        wipFiber.alternate.hooks &&
+        wipFiber.alternate.hooks[hookIndex]
+    const hook = {
+        state: oldHook ? oldHook.state : initial,
+        queue: [],
+    }
+
+    const actions = oldHook ? oldHook.queue : []
+    actions.forEach(action => {
+        hook.state = action(hook.state)
+    })
+
+    const setState = action => {
+        hook.queue.push(action)
+        wipRoot = {
+            dom: currentRoot.dom,
+            props: currentRoot.props,
+            alternate: currentRoot,
+        }
+        nextUnitOfWork = wipRoot
+        deletions = []
+    }
+
+    wipFiber.hooks.push(hook)
+    hookIndex++
+    return [hook.state, setState]
+}
+const Didact = {
+    createElement,
+    render,
+    useState,
+}
+
+/** @jsx Didact.createElement */
+function Counter() {
+    const [state, setState] = Didact.useState(1)
+    return (
+        <h1 onClick={() => setState(c => c + 1)}>
+            Count: {state}
+        </h1>
+    )
+}
+const element = <Counter />
+const container = document.getElementById("root")
+Didact.render(element, container)
